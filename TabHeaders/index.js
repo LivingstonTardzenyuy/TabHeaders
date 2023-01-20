6877d7b090a8d850356c4715c9dc1fb918e8d019
Kongnyuy Livingston <112919571+LivingstonTardzenyuy@users.noreply.github.com>
2023-01-21 00:29:56 +0100

    Tabheaders using html
    Tab headers

---
+++ TabHeaders/index.js
@@ -0,0 +1,9 @@
+function openCity(cityName, element, color)
+{
+    var i,tabcontent,tablink;
+    tabcontent=document.getElementById("tabcontent");
+    for(i=0; i<tabcontent.length;i++)
+    {
+        tabcontent[i].style.backgroundColor="";
+    }
+}
